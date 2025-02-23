5432e5307a3e43e2c9427b5bba743adb83245947
Nicholas Pimenta <nicholas2ilva@gmail.com>
2025-02-23 16:44:12 -0300

    ensure cache is revalidated when creating an order

--- src/app/[slug]/menu/actions/createOrder.ts
+++ src/app/[slug]/menu/actions/createOrder.ts
@@ -4,6 +4,7 @@ import { db } from "@/lib/prisma"
 import { OrderConsumptionMethod } from "@prisma/client"
 import { removeCpfPunctuation } from "../helpers/cpf"
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 interface CreateOrderInput {
   customerName: string;
@@ -55,5 +56,6 @@ export const createOrder = async (input : CreateOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
-  redirect(`/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`)
+  revalidatePath(`/${input.slug}/orders`);
+  redirect(`/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`);
 }
